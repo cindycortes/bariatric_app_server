1b7d6f0554f4c83e369308d07fb2d7a465ec8386
Cindy Cortes <cindy.indins@gmail.com>
2019-02-21 14:07:22 -0700

    fixed error

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/', require('./routes/indexRoutes'))
+// app.use('/', require('./routes/indexRoutes'))
 app.use('/', require('./config/routes'))
 
 app.listen(port, function() {
